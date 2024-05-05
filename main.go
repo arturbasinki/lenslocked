fd59975e55e9f2b7cccbfef63bb81375e0d6e72d
arturbasinki <artur.basinski@gmail.com>
2024-05-05 21:45:32 +0200

    adding simple routing using http package

--- main.go
+++ main.go
@@ -5,12 +5,56 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+type Router struct{}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Hello, Artur mój Panie!!!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please email us at <a href=\"mailto:q5QpN@example.com\">q5QpN@example.com</a>!</p>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ Page</h1><p>Please check our <a href=\"/contact\">contact page</a> for details.</p>")
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	// http.HandleFunc("/", pathHandler)
+	var router Router
+	// var router http.HandlerFunc
+	// router := http.HandlerFunc(pathHandler)
 	fmt.Println("Starting the srver on :3000...")
-	http.ListenAndServe(":3000", nil)
+	// http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", &router)
 }
