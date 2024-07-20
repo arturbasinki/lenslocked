220d3587e95e1e3043b004247e9b7e3783893ae9
arturbasinki <artur.basinski@gmail.com>
2024-07-20 22:33:48 +0200

    cleaning up a main.go

--- main.go
+++ main.go
@@ -11,22 +11,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-// type Router struct{}
-
-// func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	case "/faq":
-// 		faqHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page not found", http.StatusNotFound)
-// 	}
-
-// }
-
 func executeTemplate(w http.ResponseWriter, filepath string) {
 	tpl, err := views.Parse(filepath)
 	if err != nil {
@@ -56,17 +40,6 @@ func galeryHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("<h1>Gallery ID: %s</h1>", galleryID)))
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page not found", http.StatusNotFound)
-// 	}
-// }
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -77,14 +50,6 @@ func main() {
 	r.NotFound(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}))
-	// http.HandleFunc("/", homeHandler)
-	// http.HandleFunc("/contact", contactHandler)
-	// http.HandleFunc("/", pathHandler)
-	// var router Router
-	// var router http.HandlerFunc
-	// router := http.HandlerFunc(pathHandler)
 	fmt.Println("Starting the server on :3050...")
-	// http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
-	// http.ListenAndServe(":3000", &router)
 	http.ListenAndServe(":3050", r)
 }
